8e818d3cab19198ac501064d9396e45f8dc65308
kross <662242@mail.ru>
2020-04-22 17:04:54 +0500

    lesson19 animation

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
 	const countTimer = dedline => {
@@ -36,6 +37,66 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 		apDetClock();
 	};
-	countTimer('22 aprel 2020');
+	//countTimer('23 aprel 2020');
+	//......menu..............
+	const toglMenu = () => {
+		const btnMenu = document.querySelector('.menu'),
+			menu = document.querySelector('menu'),
+			menuItem = menu.querySelectorAll('ul>li'),
+			clousBtn = document.querySelector('.close-btn');
+		const handlerMenu = () => {
+			menu.classList.toggle('active-menu');
+		};
+		btnMenu.addEventListener('click', handlerMenu);
+		clousBtn.addEventListener('click', handlerMenu);
+		menuItem.forEach(element => {
+			element.addEventListener('click', handlerMenu);
+		});
+	};
+	//toglMenu();
+	//..popap..
+	const togglePopup = () => {
+		const popap = document.querySelector('.popup'),
+			popupBtn = document.querySelectorAll('.popup-btn'),
+			popupClouse = document.querySelector('.popup-close'),
+			popupContent = popap.querySelector('.popup-content');
+		function printNumbers(from, to) {
+			let current = from;
+			setTimeout(function go() {
+			  console.log(current);
+			  if (current > 10) {
+				  const p1 = current;
+					popupContent.style.top = `${p1}%`;//10
+			  }
+			  if (current > 38) {
+				  const p2 = current;
+				  popupContent.style.left = `${p2}%`;//38
+			  }
+			  // eslint-disable-next-line no-mixed-spaces-and-tabs
+			  if (current > to) {
+					setTimeout(go, 8);
+	 	  }
+				current -= 2;
+			}, 90);
+		}
 
+		    // eslint-disable-next-line no-mixed-spaces-and-tabs
+		    popupBtn.forEach(element => {
+			element.addEventListener('click', () => {
+				if (document.body.clientWidth > 768) {
+					popap.style.display = 'block';
+					popupContent.style.left = `100%`;
+					popupContent.style.top = `100%`;
+					  printNumbers(100, 10);
+					  console.log(document.body.clientWidth);
+				} else {
+					popap.style.display = 'block';
+				}
+			});
+		});
+		popupClouse.addEventListener('click', () => {
+			popap.style.display = 'none';
+		});
+	};
+	togglePopup();
 });
